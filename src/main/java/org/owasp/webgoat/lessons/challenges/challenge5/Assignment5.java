ab6387c192c03e56aabe4893e8ebecbf31452635
V1P3R3S <aleaguirre2004@gmail.com>
2026-02-26 18:01:11 +0100

    VulnSQL

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -48,27 +48,28 @@ public class Assignment5 extends AssignmentEndpoint {
   @ResponseBody
   public AttackResult login(
       @RequestParam String username_login, @RequestParam String password_login) throws Exception {
-    if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
-      return failed(this).feedback("required4").build();
-    }
-    if (!"Larry".equals(username_login)) {
-      return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
-    }
-    try (var connection = dataSource.getConnection()) {
-      PreparedStatement statement =
-          connection.prepareStatement(
-              "select password from challenge_users where userid = '"
-                  + username_login
-                  + "' and password = '"
-                  + password_login
-                  + "'");
-      ResultSet resultSet = statement.executeQuery();
-
-      if (resultSet.next()) {
-        return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
-      } else {
-        return failed(this).feedback("challenge.close").build();
+      if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
+          return failed(this).feedback("required4").build();
       }
-    }
+      if (!"Larry".equals(username_login)) {
+          return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
+      }
+      try (var connection = dataSource.getConnection()) {
+          String query = "select password from challenge_users where userid = ? and password = ? " ;
+
+          try (PreparedStatement statement = connection.prepareStatement(query)) {
+              statement.setString(1, username_login);
+              statement.setString(2, password_login);
+
+              ResultSet resultSet = statement.executeQuery(query);
+
+
+              if (resultSet.next()) {
+                  return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+              } else {
+                  return failed(this).feedback("challenge.close").build();
+              }
+          }
+      }
   }
 }
